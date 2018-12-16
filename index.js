5ecababfaccccc6c2f7c964c5ffce5be49545884
luc <luctunechi45@gmail.com>
2018-12-16 23:50:23 +0200

    Models in mongodb

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ mongoose
   .catch(err => console.error("could not connect to MongoDB..", err))
 
 const courseSchema = new mongoose.Schema({
+  // once we have a schema, we compile to model down witch gives us a Class (Course)
   name: String,
   author: String,
   tags: [string],
   date: { type: Date, default: Date.now },
   isPublished: Boolean
+})
+
+const Course = mongoose.model("Course", courseSchema)
+const course = new Course({
+  // create object based on class(Course) and store in DB
+  name: "Node.js Course",
+  author: "Luc",
+  tags: ["node", "backend"],
+  isPublished: true
 })
