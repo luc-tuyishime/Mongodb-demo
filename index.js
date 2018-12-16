d282d181db51ba1aad0adfd29c5a7f6d6ca56ce7
luc <luctunechi45@gmail.com>
2018-12-17 00:28:01 +0200

    Querying or getting Documents

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function createCourse() {
     // create object based on class(Course) and store in DB
     name: "Angular Course",
     author: "Luc",
-    tags: ["angular", "frontend"],
+    tags: ["angular", "frontend dev"],
     isPublished: true
   })
 
@@ -29,4 +29,12 @@ async function createCourse() {
   console.log(result)
 }
 
-createCourse()
+async function getCourses() {
+  const courses = await Course.find({ author: "Luc", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 })
+  console.log(courses)
+}
+
+getCourses()
