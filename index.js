6edad8d49605118f618181e687bba91da75e532a
luc <luctunechi45@gmail.com>
2018-12-24 16:24:12 +0200

    query operators

--- index.js
+++ index.js
@@ -30,7 +30,30 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: "Luc", isPublished: true })
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+  // or and operators
+  const courses = await Course
+    // .find({ price: { $gt: 10, $lte: 20 }}) // quering document using operators
+    // .find({ price: { $in: [10, 20, 30] }})
+    // .find().or([ { author: 'Mosh' }, { isPublished: true }])
+
+    // start with mosh
+    // .find({ author: /^Mosh/ })
+
+    // End with Hamadani
+    // .find({ author: /Hamadani$/ })
+
+    // Contain mosh
+    // .find({ author: /.*Mosh*./i })
+    // .count() to count the number of documents
+
+    .find({ author: "Luc", isPublished: true })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
